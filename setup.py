be95e107d1f3c2c3c247010928892857da2e9bf7
d33p0st <d33p0st@gmail.com>
2025-04-09 07:52:49 +0530

    Update: added .so and .pyd files in source itself

--- setup.py
+++
@@ -1,7 +0,0 @@
-
-import os
-import pathlib
-
-os.system(f"python -m pip install -U --force-reinstall \"https://github.com/Nuitka/Nuitka/archive/factory.zip\"")
-os.system(f"nuitka --module {pathlib.Path('./src/lawyer/parsers.py').expanduser().resolve()} --output-dir={pathlib.Path('./src/lawyer').resolve()} --jobs=10 --lto=yes --python-flag=no_site --python-flag=no_warnings --remove-output --full-compat --prefer-source-code --warn-implicit-exceptions --warn-unusual-code --no-pyi-file --assume-yes-for-downloads")
-os.system(f"nuitka --module {pathlib.Path('./src/lawyer/exceptions.py').expanduser().resolve()} --output-dir={pathlib.Path('./src/lawyer').resolve()} --jobs=10 --lto=yes --python-flag=no_site --python-flag=no_warnings --remove-output --full-compat --prefer-source-code --warn-implicit-exceptions --warn-unusual-code --no-pyi-file --assume-yes-for-downloads")

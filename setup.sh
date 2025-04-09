d3f5f331e8249fbe9a6580fcfede9138df0bb3b2
d33p0st <d33p0st@gmail.com>
2025-04-09 05:47:34 +0530

    Update: added build workflow

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python -m pip install -U --force-reinstall "https://github.com/Nuitka/Nuitka/archive/factory.zip"
-nuitka --module backups/parsers.py --output-dir=src/lawyer --jobs=10 --lto=yes --python-flag=no_site --python-flag=no_warnings --remove-output --full-compat --prefer-source-code --warn-implicit-exceptions --warn-unusual-code --no-pyi-file
-nuitka --module backups/exceptions.py --output-dir=src/lawyer --jobs=10 --lto=yes --python-flag=no_site --python-flag=no_warnings --remove-output --full-compat --prefer-source-code --warn-implicit-exceptions --warn-unusual-code --no-pyi-file
+nuitka --module backups/parsers.py --output-dir=src/lawyer --jobs=10 --lto=yes --python-flag=no_site --python-flag=no_warnings --remove-output --full-compat --prefer-source-code --warn-implicit-exceptions --warn-unusual-code --no-pyi-file --assume-yes-for-downloads
+nuitka --module backups/exceptions.py --output-dir=src/lawyer --jobs=10 --lto=yes --python-flag=no_site --python-flag=no_warnings --remove-output --full-compat --prefer-source-code --warn-implicit-exceptions --warn-unusual-code --no-pyi-file --assume-yes-for-downloads
 pip install .
 rm -rf build src/lawyer.egg-info
